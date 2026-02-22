0a8446b936b7e5e4fd4ad0f26302f0cb31d682de
Moritisimor <devMoritisimor@proton.me>
2026-02-22 17:01:33 +0100

    Fixed Pydantic data model and added basic functionality for downloading generated speech.

--- src/requestbody.py
+++ src/requestbody.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel
 class RequestBody(BaseModel):
-    def __init__(self, voice: int, volume: float, rate: int, text: str):
-        self.voice = voice
-        self.volume = volume
-        self.rate = rate
-        self.text = text
+    voice: int
+    volume: float | int
+    rate: int
+    text: str
---
+++ src/static/script.js
@@ -0,0 +1,36 @@
+let textTextarea = document.getElementById("textTextarea");
+let sendButton = document.getElementById("sendButton");
+
+sendButton.addEventListener("click", () => {
+    let url = window.location.href;
+    fetch(
+        `${url}api/speak`, 
+        {
+            "headers": new Headers({"content-type": "application/json"}),
+            "method": "POST",
+            "body": JSON.stringify({
+                "voice": 0,
+                "rate": 200,
+                "volume": 1.0,
+                "text": textTextarea.value
+            })
+        }
+    ).then(res => {
+        if (!res.ok) {
+            throw new Error("");
+        } 
+
+        return res.blob();
+    }).then(blob => {
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+
+        a.href = url;
+        a.download = "OpenTTS.wav";
+
+        a.click();
+        a.remove();
+    }).catch(err => {
+        alert(err);
+    })
+});
