144bc288d2d688f92673f5ac8b3a1998902640a5
Moritisimor <devMoritisimor@proton.me>
2026-02-22 12:39:06 +0100

    Added request handler for generating Speech from Text

--- src/main.py
+++ src/main.py
@@ -1,10 +1,25 @@
 import fastapi
 from fastapi import staticfiles
+from fastapi.responses import StreamingResponse
 import uvicorn
 import pyttsx3
+import io
+import tempfile
 
-engine = pyttsx3.Engine()
 app = fastapi.FastAPI()
+
+@app.get("/api/speak/{text}")
+async def speak(text: str):
+    with tempfile.NamedTemporaryFile(mode="w+b", delete=True, suffix=".wav") as f:
+        engine = pyttsx3.init()
+        engine.save_to_file(text, f.name)
+        engine.runAndWait()
+
+        f.flush()
+        f.seek(0)
+
+        buf = io.BytesIO(f.read())
+        return StreamingResponse(buf)
 
 app.mount("/", staticfiles.StaticFiles(directory="./static", html=True))
 
